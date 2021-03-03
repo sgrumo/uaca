05980df5fbef12785acebfc5b20db8b55efbcba8
Samuele Medici <samuele.medici@quinck.io>
2021-03-03 20:41:11 +0100

    test init

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>UACA?</h1>
+        <h1>AH YEAH</h1>
+        <iframe width="100%" height="640"
+          frameborder="0" allowfullscreen allow="xr-spatial-tracking; gyroscope; accelerometer"
+          scrolling="no" src="https://kuula.co/share/collection/7Yl5l?fs=1&vr=1&sd=1&initload=0&thumbs=1&info=0&logo=-1"></iframe>
       </header>
     </div>
   );
