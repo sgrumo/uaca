d3acccdbc896f7f05e12b0954a17acab986c55cc
Samuele Medici <samuele.medici@quinck.io>
2021-03-03 20:43:53 +0100

    added title to iframe

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1>AH YEAH</h1>
         <iframe width="100%" height="640"
+          title="kuulu"
           frameborder="0" allowfullscreen allow="xr-spatial-tracking; gyroscope; accelerometer"
           scrolling="no" src="https://kuula.co/share/collection/7Yl5l?fs=1&vr=1&sd=1&initload=0&thumbs=1&info=0&logo=-1"></iframe>
       </header>
